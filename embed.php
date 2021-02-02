7ca7b7be706b27707434d5c42952ef0c8044c14b
viperspolite <50474059+viperspolite@users.noreply.github.com>
2021-02-02 14:03:55 +0530

    Update embed.php

--- embed.php
+++ embed.php
@@ -44,13 +44,13 @@ $title1 = $second_title[0];
   <div id="myElement"></div>
       <br />
       <center>
-		<img height="80%" width="60%" src="https://i.ibb.co/LRC4CQg/Copy-of-MOD-APK-1.png"/>
+		<img height="80%" width="60%" src="https://i.ibb.co/s1JDgWk/Copy-of-MOD-APK.png"/>
       
     <br />
 	<script src="bin/jwplayer-7.3.6/jwplayer.js"></script>	
 	<link href="bin/skins/thin.min.css" rel="stylesheet" type="text/css" />
 	<script type="text/javascript">jwplayer.key="Ywok59g9j93GtuSU7+axNzjIp/TBfiK4s0vvYg==";</script>
-    <style type="text/css">*{margin:0;padding:0}#container{position:absolute;width:100%!important;height:65%!important}</style>
+    <style type="text/css">*{margin:0;padding:0}#container{position:absolute;width:100%!important;height:60%!important}</style>
       <br />
 	<p><a href="https://telegram.me/Streamable_cinemas">
 <img src="https://lh3.googleusercontent.com/-T8I357fL-eA/YBkJvmaCQpI/AAAAAAAADrY/wwYLrIqLFOkwf658ZLiFReTLm64Wc_dhgCLcBGAsYHQ/h85/join-our-telegram-channel-300x71.png" alt="Join telegram channel" width="300" height="71">
@@ -70,12 +70,6 @@ $title1 = $second_title[0];
       },
       });
     </script>
-	      <br />
-	<p><a href="https://telegram.me/Streamable_cinemas">
-<img src="https://lh3.googleusercontent.com/-T8I357fL-eA/YBkJvmaCQpI/AAAAAAAADrY/wwYLrIqLFOkwf658ZLiFReTLm64Wc_dhgCLcBGAsYHQ/h85/join-our-telegram-channel-300x71.png" alt="Join telegram channel" width="300" height="71">
-</a></p>
-
-      <br />
   </center>	      
   </body>
 </html>
